aa30f5c5af3bb747b9073aa9d3556e6d47a2b63e
MatheusCarneir0 <matheusthayna082@gmail.com>
2024-11-05 16:11:32 -0300

    no message

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,10 +5,69 @@ class LoginForms(forms.Form):
         label="Nome de Login",
         required=True,
         max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Ex: Gabriel Tavares"
+            }
+        )
     )
     senha=forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Ex.: Gabriel Tavares"
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Ex.: joao1234@gamil.com"
+            }
+        )
+    )
+
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class":"form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
     )
